12d61c38301f172668f416437bf1f0c739bd7cb0
GABRIEL <g27r02@gmail.com>
2023-12-22 13:38:54 -0600

    CORRECCIONES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { PanelModule } from './pages/panel/panel.module';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginModule } from './pages/login/login.module';
-import { FaqRoutingModule } from './pages/panel/faq/faq-routing.module';
 
 
 @NgModule({
@@ -20,7 +19,6 @@ import { FaqRoutingModule } from './pages/panel/faq/faq-routing.module';
     AppRoutingModule,
     PanelModule,
     LoginModule,
-    FaqRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/panel/usuarios/usuarios-routing.module.ts
+++ src/app/pages/panel/usuarios/usuarios-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuariosComponent } from './usuarios.component';
-import { AuthGuard } from 'src/app/core/services/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: UsuariosComponent,
-    canActivate: [AuthGuard], data: { claimType: 'CanAccessUsuarios'}
   }
 ];
 
