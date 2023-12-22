776b0aab7e3eebf50f4022944275771d6fdb46a0
carretoC <carreto154@gmail.com>
2023-12-22 17:51:40 -0600

    areas-mapa

--- src/app/pages/panel/panel-routing.module.ts
+++ src/app/pages/panel/panel-routing.module.ts
@@ -20,6 +20,16 @@ const routes: Routes = [
         loadChildren: () => import('./beneficiarios/beneficiarios.module')
           .then(u => u.BeneficiariosModule)
       },
+      {
+        path: 'areas',
+        loadChildren: () => import('./areas/areas.module')
+          .then(a => a.AreasModule)
+      },
+      {
+        path: 'mapa',
+        loadChildren: () => import('./mapa-programas-sociales/mapa-programas-sociales.module')
+          .then(map => map.MapaProgramasSocialesModule)
+      },
     ]
   }
 ];
